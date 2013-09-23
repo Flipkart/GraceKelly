a2c5c5a87d611876f1228d6968966b81fc4a1de2
suman.karthik <suman.karthik@flipkart.com>
2013-09-23 17:19:59 +0530

    re-organized code into different packages

---
+++ cache-providers/src/main/java/lego/gracekelly/couchbase/CouchBaseClientFactory.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2013 Flipkart Internet, pvt ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package lego.gracekelly.couchbase;
+
+
+import com.couchbase.client.CouchbaseClient;
+import com.couchbase.client.CouchbaseConnectionFactoryBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Creates a CouchbaseClient
+ */
+public class CouchBaseClientFactory {
+
+    /**
+     * Creates and returns {@link CouchbaseClient} based on the parameters provided.
+     * @param urls
+     * @param bucketName
+     * @param password
+     * @return {@link CouchbaseClient} instance.
+     * @throws IOException
+     */
+    public static CouchbaseClient getCouchBaseClient(String[] urls, String bucketName, String password) throws IOException {
+
+        List<URI> uris = new ArrayList<URI>();
+        for (String url: urls){
+            uris.add(URI.create(url));
+        }
+
+        CouchbaseConnectionFactoryBuilder builder = new CouchbaseConnectionFactoryBuilder();
+        CouchbaseClient couchbaseClient = new CouchbaseClient(builder.buildCouchbaseConnection(uris,bucketName,password));
+        return couchbaseClient;
+    }
+
+}
---
+++ core/src/test/java/lego/gracekelly/CacheEntryTest.java
@@ -0,0 +1,41 @@
+package lego.gracekelly; /**
+ * Copyright 2013 Flipkart Internet, pvt ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import lego.gracekelly.entities.CacheEntry;
+import org.testng.annotations.Test;
+
+public class CacheEntryTest {
+
+    @Test
+    public void cacheEntryWithTTL(){
+        CacheEntry  cacheEntry = new CacheEntry("dude","suman",300);
+
+        assert cacheEntry.getValue().equals("suman");
+        assert cacheEntry.getKey().equals("dude");
+        assert cacheEntry.getTtl() == 300;
+        assert cacheEntry.getEpoch_timestamp()>0;
+    }
+
+    @Test
+    public void cacheEntryWithoutTTL(){
+        CacheEntry  cacheEntry = new CacheEntry("dude","suman");
+        assert cacheEntry.getValue().equals("suman");
+        assert cacheEntry.getKey().equals("dude");
+        assert cacheEntry.getTtl() == 0;
+        assert cacheEntry.getEpoch_timestamp()==0;
+    }
+
+}
